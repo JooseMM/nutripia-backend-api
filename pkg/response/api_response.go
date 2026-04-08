e95fd4ac6146098edb09098b09f0932ac8af028e
Jose Moreno <jose.moreno@ttp.local>
2026-04-08 10:20:27 -0400

    refactor: add writeJSON wrapper

--- pkg/response/api_response.go
+++ pkg/response/api_response.go
@@ -1,6 +1,19 @@
 package response
 
+import (
+	"encoding/json"
+	"net/http"
+)
+
 type ApiResponse[T any] struct {
-	Success bool   `json:"success"`
-	Data    *T      `json:"data"`
+	Success bool `json:"success"`
+	Data    *T   `json:"data"`
+}
+
+func WriteJSON(w http.ResponseWriter, status uint16, data any) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(int(status))
+	if err := json.NewEncoder(w).Encode(data); err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
 }
